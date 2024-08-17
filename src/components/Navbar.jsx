ea5a5f6d88d2f816ff6e652d93b9668278d97a8a
FavourtheDev <favourthedeveloper@gmail.com>
2024-08-17 15:53:40 -0700

    Updated Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='flex flex-row justify-between bg-blue-700 p-10'>
-        <h3 className='text-5xl font-bold'>Job Listings</h3>
+    <nav className='flex flex-row justify-between bg-blue-700 p-5'>
+        <h3 className='text-4xl font-bold'>Job Listings</h3>
 
-        <div className="links flex flex-row space-x-5 text-2xl pt-2 text-yellow-300">
+        <div className="links flex flex-row space-x-5 text-xl pt-2 text-yellow-300">
             <a href="">Home</a>
             <a href="">Jobs</a>
             <a href="">Add Jobs</a>
