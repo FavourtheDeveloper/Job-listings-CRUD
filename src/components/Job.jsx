c9154b76da187e480e490b8f2f0241635bb4258d
FavourtheDev <favourthedeveloper@gmail.com>
2024-08-18 14:49:12 +0100

    Responsiveness for Jobs section

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -3,7 +3,7 @@ import Button from "./Button";
 
 const Job = () => {
   return (
-    <div className="bg-gray-200 p-4 m-2 w-1/4 rounded-xl">
+    <div className="bg-gray-200 p-4 m-2 md:w-1/4 rounded-xl">
       <small>Full Time</small>
       <h1 className="text-2xl font-bold mb-3">Senior React Developer</h1>
       <p className="text-sm">We are seeking a Senior Frontend Developer to join our Team</p>
